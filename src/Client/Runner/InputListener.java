753b4078ecb16e6b4ba2f8384c6d21ecd53a85a8
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-10-27 18:22:27 -0700

    Update InputListener.java

--- src/Client/Runner/InputListener.java
+++ src/Client/Runner/InputListener.java
@@ -1,1 +1,38 @@
+package Client.Runner;
 
+import java.io.ObjectInputStream;
+
+import Client.Main.Client;
+
+public class InputListener implements Runnable {
+	
+	private Thread thread;
+	
+	private Client client;
+	
+	public InputListener(Client client) {
+		thread=new Thread(this);
+		this.client=client;
+	}
+	
+	public synchronized void start() {
+		thread.start();
+	}
+	
+	@SuppressWarnings("deprecation")
+	public synchronized void stop() {
+		thread.stop();
+	}
+	
+	public void run() {
+		while(true) {
+			if(client.getSocket()!=null) {
+				try {
+					ObjectInputStream input=new ObjectInputStream(client.getSocket().getInputStream());
+					client.getWindow().setText(client.getWindow().getText()+String.valueOf(input.readObject())+"\n");
+					client.getWindow().getTextArea().setText(client.getWindow().getText());
+				}catch(Exception exception) {}
+			}
+		}
+	}
+}
